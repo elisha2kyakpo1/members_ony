439082616f3b9a239e670211ea14e28a83763252
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-01-23 12:14:09 +0200

    add new post view and style

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,9 +11,10 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
 
     if @post.save
-      redirect_to index_posts_path
+      redirect_to index_posts_path, notice: 'post created succussfully!'
+
     else
-      render :new
+      render :new, alert: 'The post was not created!'
     end
   end
 
