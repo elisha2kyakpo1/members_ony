17c5cba3eeaa0f97c685b856a754fee6e7de4858
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-01-26 08:12:41 +0200

    style login page

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -2,7 +2,7 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
   def acount_update_params
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_25_160721) do
+ActiveRecord::Schema.define(version: 2021_01_26_050032) do
 
   create_table "posts", force: :cascade do |t|
     t.string "Title"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2021_01_25_160721) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
