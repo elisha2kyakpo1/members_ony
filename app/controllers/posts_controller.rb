af5ee160002076d43c0e394a661db5b7c1e9e8e3
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-01-26 09:01:21 +0200

    style post view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[show edit update destroy]
+  before_action :require_login, only: %i[create update]
 
   # GET /posts
   # GET /posts.json
